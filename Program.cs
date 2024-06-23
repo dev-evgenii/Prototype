18fbec1c2436b774b1a2102e4ee914c423aee7f1
dev-evgenii <developer.evgenii@yandex.ru>
2024-06-24 00:25:06 +0300

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿
-/*
-Описание: Есть 3 класса Person, Pilot, Passenger. Эти классы реализуют наследование. 
-Базовый класс Person содержит поля Id, FirstName, LastName. От этого класса наследуются классы Pilot и Passenger.
-Каждый класс реализуют интерфейс IClonable для клонирования и обобщеный интерфейс IMyClonable<T>.
-*/
-
-using Prototype;
+﻿using Prototype;
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
